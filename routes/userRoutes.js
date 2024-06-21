38c7a12564b9de2b091b601e9aa151a8f07e3b03
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-06-21 17:53:56 +0530

    register path added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware"); // Path to your upload middleware
 
-router.route("/").post(registerUser);
+router.route("/register").post(registerUser);
 router.post("/login", authUser);
 router
   .route("/profile")
